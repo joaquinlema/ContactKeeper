e82b3e347956fb53e121c53f742dc4f7dabb8b05
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-09-22 13:08:09 -0300

    ajustamos la conexion con la database

--- config/db.js
+++ config/db.js
@@ -6,14 +6,12 @@ const connectDB = async () => {
     try {
         await mongoose.connect(db, {
             useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
         });
         console.log('Connected ....')
-    } catch (error) {
+    } catch (err) {
         console.error(err.message);
         process.exit(1);
-    }
-}
+    } 
+};
 
 module.exports = connectDB;
