0955f29f521a3bb700b0d4a9a950f68e675fd04b
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-09-23 13:55:21 -0300

    agregamos cdrear un usuario y guardarlo en mongo

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult, check } = require('express-validator');
+const bcrypt = require('bcriptjs');
 
 const User = require('../models/User');
 
@@ -11,13 +12,41 @@ router.post('/', [
     check('name', 'name is required').not().isEmpty(),
     check('email', 'Please introduce a correct email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body);
+
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email: email });
+        if (user) {
+            return res.status(400).json({ msg: 'User already exist' });
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt); //hash version of password
+
+        await user.save();
+
+        res.send('User save');
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+
+    //    res.send(req.body);
 });
 
 module.exports = router;
