1750a40b5e5a8d55ebb69442e1d4a5b9733641b7
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-09-29 20:06:34 -0300

    agregamos update

--- routes/contacts.js
+++ routes/contacts.js
@@ -29,10 +29,10 @@ router.post('/', [auth, [check('name', 'name is required').not().isEmpty(),]], a
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const {name, email, phone, type} = req.body;
+    const { name, email, phone, type } = req.body;
 
     try {
-        const newContact = new Contact({name, email, phone, type, user: req.user.id});
+        const newContact = new Contact({ name, email, phone, type, user: req.user.id });
 
         //guardamos el contacto en la base
         const contact = await newContact.save();
@@ -48,8 +48,36 @@ router.post('/', [auth, [check('name', 'name is required').not().isEmpty(),]], a
 //@route PUT api/contacts/:id
 //@desc update contact 
 //@access Private
-router.put('/:id', (req, res) => {
-    res.send('update contact');
+router.put('/:id', auth, async (req, res) => {
+    const { name, email, phone, type } = req.body;
+
+    // build contact object
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+
+    try {
+        //INFO: de esta forma se puede consutlar los params de la copnsuilta
+        let contact = await Contact.findById(req.params.id);
+        if(!contact) return res.status(400).json({msg: 'Contact not found'});
+
+        //WARNING: asegurarse que el uyser tenga contacts
+        if(contact.user.toString() !== req.user.id){
+            return res.status(401).json({msg: 'not authorized'});
+        }
+
+        //INFO: buscamos el contacto por id y le seteamos contactfields si no existe lo creamos
+        contact = await Contact.findByIdAndUpdate(req.params.id, {$set : contactFields},{new: true});
+
+        res.json(contact);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error');
+    }
+
 });
 
 //@route DELETE api/contacts/:id
