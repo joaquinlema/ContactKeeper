152efb27699cb6c4a9becc8e7567d8fe951f4f55
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-10-06 22:06:50 -0300

    agregamos las rutas para auth

--- client/src/service/Auth.Service.js
+++ client/src/service/Auth.Service.js
@@ -1,33 +1,18 @@
 import http from "../helpers/axiosInstance";
 
-const seccion = 'Auth';
+const seccion = 'auth';
 
 const getAll = () => {
   return http.get(`/${seccion}`);
-};
-
-const getById = id => {
-  return http.get(`/${seccion}/getById?Id=${id}`);
 };
 
 const create = data => {
   return http.post(`/${seccion}`, data);
 };
 
-const update = (data) => {
-  return http.post(`/${seccion}/edit`,data);
-};
-
-const remove = (data) => {
-  return http.post(`/${seccion}/deleteById`, data);
-};
-
 const AuthService = {
   getAll,
-  getById,
   create,
-  update,
-  remove,
 };
 
 export default AuthService;
