8aa65052f0f32cdba5aa07d60b3b7716e293b8d1
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-10-06 22:17:27 -0300

    ajustamos los llamados al servicio

--- client/src/service/Contacts.Service.js
+++ client/src/service/Contacts.Service.js
@@ -2,32 +2,39 @@ import http from "../helpers/axiosInstance";
 
 const seccion = 'contacts';
 
+const config = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
 const getAll = () => {
-  return http.get(`/${seccion}`);
+    return http.get(`/${seccion}`);
 };
 
 const getById = id => {
-  return http.get(`/${seccion}/getById?Id=${id}`);
+    return http.get(`/${seccion}/getById?Id=${id}`);
 };
 
 const create = data => {
-  return http.post(`/${seccion}`, data);
+
+    return http.post(`/${seccion}`, data, config);
 };
 
 const update = (data) => {
-  return http.put(`/${seccion}/:id`,data);
+    return http.put(`/${seccion}/:id`, data, config);
 };
 
 const remove = (data) => {
-  return http.delete(`/${seccion}/:id`, data);
+    return http.delete(`/${seccion}/:id`, data);
 };
 
 const ContactService = {
-  getAll,
-  getById,
-  create,
-  update,
-  remove,
+    getAll,
+    getById,
+    create,
+    update,
+    remove,
 };
 
 export default ContactService;
