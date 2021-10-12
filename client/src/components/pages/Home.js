2214c732047ebb676dff224810d1532c82211f3d
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-10-12 15:54:02 -0300

    agrgeamos el fomrulario a la home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useContext,useEffect } from 'react';
 import { Row, Col } from 'antd';
 import Contacts from '../contacts/Contacts';
 import ContactContext from '../../context/contact/ContactContext';
+import ContactForm from '../contacts/ContactForm';
 
 const Home = () => {
 
@@ -15,7 +16,7 @@ const Home = () => {
     return (
         <Row>
             <Col xs={10} sm={4} md={8} lg={12} xl={12}>
-                
+                <ContactForm />
             </Col>
             <Col xs={14} sm={20} md={16} lg={12} xl={12}>
                <Contacts />
