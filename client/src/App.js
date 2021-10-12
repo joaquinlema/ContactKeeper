8e257ff0ed04be751e927173fc11c2b3d904d0f1
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-10-12 18:25:46 -0300

    agrgeamos alert context

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,16 @@ import "antd/dist/antd.css";
 import LandingPage from './components/pages/LandingPage';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
   return (
     <AuthState>
+      <AlertState>
       <ContactState>
         <LandingPage />
       </ContactState>
+      </AlertState>
     </AuthState>
   );
 }
